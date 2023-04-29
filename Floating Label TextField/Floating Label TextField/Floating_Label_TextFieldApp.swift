508272059c8ce8794a1515783116d4d7ac1e3af2
Elaidzha1940 <eli080293@gmail.com>
2023-04-29 23:54:34 +0700

    Floating Label TextField

---
+++ Floating Label TextField/Floating Label TextField/Floating_Label_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Floating_Label_TextFieldApp.swift
+//  Floating Label TextField
+//
+//  Created by Elaidzha Shchukin on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Floating_Label_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
